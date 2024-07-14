6aae5c50c59e3e2e765028b33968028fa47a2f73
Carlos Aguilera <ckarlos_95@hotmail.es>
2024-07-14 11:45:02 -0600

    Use GTM scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager } from '@next/third-parties/google'
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <GoogleTagManager gtmId="GTM-TPBJXFSP" /> // Analytics
+      <head>
+        {/* <!-- Google Tag Manager --> */}
+        <Script>
+          {
+            `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TPBJXFSP');
+            `
+          }
+        </Script>
+        {/* <!-- End Google Tag Manager --> */}
+      </head>
+
       <body className={classNames(inter.className, 'bg-white')}>
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPBJXFSP"
+          height="0" width="0" className="hidden invisible"></iframe></noscript>
+
         <header>
           <NavBar />
         </header>
